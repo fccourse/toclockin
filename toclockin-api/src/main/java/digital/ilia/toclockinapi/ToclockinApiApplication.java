72a0e0488b252cb6b9e4f5f7da5c45fcf3fc35fe
Pedro Ulhoa <pedroulhoa@live.com>
2021-10-29 12:15:26 -0300

    initial project setup

---
+++ toclockin-api/src/main/java/digital/ilia/toclockinapi/ToclockinApiApplication.java
@@ -0,0 +1,13 @@
+package digital.ilia.toclockinapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToclockinApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToclockinApiApplication.class, args);
+    }
+
+}
